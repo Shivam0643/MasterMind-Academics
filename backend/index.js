76e9fabb59b4f3b65c1f6fd1a3ad06989832943d
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-01-08 21:17:23 +0530

    till done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
+import { v2 as cloudinary } from 'cloudinary';
+import courseRoute from './routes/course.route.js'
+import fileUpload from "express-fileupload";
 
 const app = express();
 dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}))
+
 
 const PORT = process.env.PORT || 4000;
 const DB_URI = process.env.MONGO_URI
@@ -15,9 +26,15 @@ try {
     console.log(error);
 }
 
-app.get('/', (req, res) => {
-    res.send(`Server is running at port ${PORT}`)
-})
+// Defining routes
+app.use("/api/v1/course", courseRoute);
+
+// cloudinary configuration code
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
