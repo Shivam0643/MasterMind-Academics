00fb7d523841a707b69aa3e00800a5470736eafb
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-04-02 19:22:07 +0530

    testing the deployment

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -3,4 +3,4 @@
 export const BACKEND_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:3000/api/v1" // Local Backend
-        : "https://mastermind-academix-1.onrender.com/api/v1"; // Render Backend
+        : "https://mastermind-academix.onrender.com/api/v1"; // Render Backend
