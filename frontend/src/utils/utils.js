39c6a2c72147904d53738e70686ccca431504112
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-04-02 15:12:44 +0530

    problem in backend url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,6 @@
-export const BACKEND_URL = "https://master-mind-academix.vercel.app/api/v1";
+// export const BACKEND_URL = "http://localhost:3000/api/v1";
+
+export const BACKEND_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:3000/api/v1" // Local Backend
+        : "https://mastermind-backend.vercel.app/api/v1"; // Deployed Backend
