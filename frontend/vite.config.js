712e6444a7875d0abdc1322f36605badebe7f0c0
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-04-02 19:02:56 +0530

    testing the deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
-  }
-});
+  base: "/", // Ensures the app is served from the root
+})
