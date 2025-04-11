02037430a683ffd029ac9ed57b3806c2bf30e1e1
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2025-04-11 18:23:57 +0530

    fixed admin login problem

--- frontend/src/Admin/ProtectedAdminRoute.jsx
+++ frontend/src/Admin/ProtectedAdminRoute.jsx
@@ -2,16 +2,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import React from "react";
 
 const ProtectedAdminRoute = () => {
-    const admin = JSON.parse(localStorage.getItem("admin"));
-    console.log("Admin Data:", admin);  // Debugging
+    const token = localStorage.getItem("adminToken");
+    console.log("Token:", token);  // Debugging
 
-    if (!admin || !admin.token) {
+    if (!token) {
         console.log("Unauthorized - Redirecting to login");
         return <Navigate to="/admin/login" replace />;
     }
 
     return <Outlet />;
 };
-
 
 export default ProtectedAdminRoute;
